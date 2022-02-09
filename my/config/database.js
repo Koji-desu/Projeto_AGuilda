92127ba31d178b7eb0f496d01afeb45a043c79ad
Koji-Desu <pdigo153@gmail.com>
2022-02-09 15:55:28 -0300

    Sequelize instalado

---
+++ my/config/database.js
@@ -0,0 +1,10 @@
+const config = {
+    username: 'root', 
+    password: '', 
+    database: 'nodejs', 
+    host: 'localhost',
+    port: '3306',
+    dialect: 'mysql'
+}
+
+module.exports = config
