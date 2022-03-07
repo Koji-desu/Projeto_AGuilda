410bf4426d26e825ddedb6900bb21b84923aa198
Koji-Desu <pdigo153@gmail.com>
2022-03-07 07:52:37 -0300

    .:memo:

--- config/database.js
+++ config/database.js
@@ -1,10 +1,11 @@
-const config = {
-    username: 'root', 
-    password: '', 
-    database: '', 
-    host: '127.0.0.1',
-    port: '3306',
-    dialect: 'mysql'
+require('dotenv').config()
+
+module.exports = {
+    username: 'process.env.DB_USER', 
+    password: 'process.env.DB_PASS', 
+    database: 'process.env.DB_NAME', 
+    host: 'process.env.DB_HOST',
+    port: 'process.env.DB_PORT',
+    dialect: 'process.env.DB.DIALECT'
 }
 
-module.exports = config
